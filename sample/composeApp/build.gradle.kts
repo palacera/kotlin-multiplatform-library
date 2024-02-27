12477ccf700ee5435f9cd99576852f59a9ab041f
Andrew Keller <578504+aiithak@users.noreply.github.com>
2024-02-27 15:27:50 -0300

    Upgrade Kotlin and Compose (#16)
    Co-authored-by: aiithak <andrewskeller@gmaill.com>

--- sample/composeApp/build.gradle.kts
+++ sample/composeApp/build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.compose.ExperimentalComposeLibrary
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
@@ -18,6 +19,12 @@ kotlin {
         browser {
             commonWebpackConfig {
                 outputFileName = "composeApp.js"
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(project.projectDir.path)
+                    }
+                }
             }
         }
         binaries.executable()
@@ -52,6 +59,7 @@ kotlin {
             implementation(compose.ui)
             @OptIn(ExperimentalComposeLibrary::class)
             implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
             implementation(projects.sample.shared)
             implementation(projects.myLibrary)
         }
